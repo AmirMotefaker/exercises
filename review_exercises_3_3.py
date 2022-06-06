29cc7ef1eaf5eb4b21e3b6fa02b8a257ba0393d4
Amir Motefaker <amotef@gmail.com>
2022-06-06 12:31:39 +0430

    Create review_exercises_3_3.py

---
+++ review_exercises_3_3.py
@@ -0,0 +1,21 @@
+# Code by @Amir Motefaker
+# Review Exercises Chapter 3 #3
+
+# 3. Write a program that changes a variable
+# several times during the program and use
+# it at different times.
+
+a = 10
+b = 100
+
+d = a + b
+
+print(d)
+
+a = 4
+print( d * 4)
+
+a = 1
+b = 2
+d = 3
+print(a + b + d)
