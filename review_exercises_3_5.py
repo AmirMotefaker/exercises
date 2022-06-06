965e8e73028be93b9e0be8dcdb63f6224e03b135
Amir Motefaker <amotef@gmail.com>
2022-06-06 14:43:16 +0430

    Create review_exercises_3_5.py

---
+++ review_exercises_3_5.py
@@ -0,0 +1,18 @@
+# Code by @Amir Motefaker
+# Review Exercises Chapter 3 #5
+
+# 5. Write a program that takes two numbers from work.
+# Then store the sum of these two numbers in a variable 
+# and multiply these two numbers by another variable.
+# Finally, the sum of these two results is displayed to the user.
+
+
+# student_name # Snake case
+
+input_a = int(input('Enter a: '))
+input_b = int(input('Enter b: '))
+
+sum_result = input_a + input_b
+mul_result = input_a * input_b
+
+print(sum_result + mul_result)
