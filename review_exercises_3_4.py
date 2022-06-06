7430b491108723051f09ca71a932fd5087ac5f9c
Amir Motefaker <amotef@gmail.com>
2022-06-06 12:41:48 +0430

    Create review_exercises_3_4.py

---
+++ review_exercises_3_4.py
@@ -0,0 +1,18 @@
+# Code by @Amir Motefaker
+# Review Exercises Chapter 3 #4
+
+# 4 - Check the following program and determine
+# the value of each variable over time.
+# Predict the outcome of this program.
+
+my_num = 2
+print(my_num * 2 + 1)
+
+c = 5
+my_num = c
+print(1 + c * my_num)
+
+my_num = my_num + 3
+# my_num += 3
+c = my_num - 1
+print(my_num / 2)
