336b4b581296722010bd4b085cd4e7692ec641b7
Amir Motefaker <amotef@gmail.com>
2022-06-06 12:24:36 +0430

    Create review_exercises_3_2.py

---
+++ review_exercises_3_2.py
@@ -0,0 +1,16 @@
+# Code by @Amir Motefaker
+# Review Exercises Chapter 3 #2
+
+# 2. Solve Question 1 using the variables.
+# Try to store the result of each calculation in one variable
+# And finally print the value of the final variable.
+
+a = 4 * 11
+b = 12 + a
+
+d = 11 / 10
+c = d ** 2
+
+result = b - c
+
+print(result)
