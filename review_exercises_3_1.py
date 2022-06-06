a0354b6249d09d35cc6367f51a8b8371f7aef855
Amir Motefaker <amotef@gmail.com>
2022-06-06 12:19:43 +0430

    Create review_exercises_3_1.py

---
+++ review_exercises_3_1.py
@@ -0,0 +1,10 @@
+# Code by @Amir Motefaker
+# Review Exercises Chapter 3 #1
+
+# 1. Write a mathematical expression that, without using 
+# a variable, performs a mathematical calculation with the "sum" operators.
+# Do "subtraction", "multiplication", "division" and "power".
+# (Without using variables)
+
+print(12 + (4 * 11) - (11 / 10) ** 2)
+print(12 / 6 + 5)
