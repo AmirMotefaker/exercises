9eb41eb9e98c6ee7e5fc5a4779d9d68315c0e186
Amir Motefaker <amotef@gmail.com>
2022-06-06 17:08:00 +0430

    Create review_exercises_3_9.py

---
+++ review_exercises_3_9.py
@@ -0,0 +1,14 @@
+# Code by @Amir Motefaker
+# Review Exercises Chapter 3 #9
+
+stu_name = 'ali'
+
+class_marks = []
+
+my_student = {
+
+}
+
+people = [{"name": "n1", "score": "s1"}, {"name": "n2", "score": "s2"}, {}]
+
+print(people[0]["name"])
