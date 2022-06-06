e591cdef5b4a1866ee07985902e4ae34357d99b6
Amir Motefaker <amotef@gmail.com>
2022-06-06 16:30:31 +0430

    Update review_exercises_3_6.py
    Added answer "Give an example that requires you to store multiple types of data."

--- review_exercises_3_6.py
+++ review_exercises_3_6.py
@@ -9,24 +9,49 @@
 #   Also have different data in them.
 
 # - Give an example where the appropriate type of data storage is a list.
-marks = [20, 11, 10, 18]
+# marks = [20, 11, 10, 18]
 
-# - Give another example where a dictionary data type is suitable for storing data.
-my_student = {
-    "name": "Ana",
-    "age": 20,
-    "lastname": "Johnson",
-    "marks": [
-        {
-            "title": "math",
-            "mark": 20
-        },
-        {
-            "title": "chem",
-            "mark": 12
-        }
-    ]
-}
+# # - Give another example where a dictionary data type is suitable for storing data.
+# my_student = {
+#     "name": "Ana",
+#     "age": 20,
+#     "lastname": "Johnson",
+#     "marks": [
+#         {
+#             "title": "math",
+#             "mark": 20
+#         },
+#         {
+#             "title": "chem",
+#             "mark": 12
+#         }
+#     ]
+# }
 
 # print(my_student['age'])
-print(my_student["marks"][1]["mark"])
+# print(my_student["marks"][1]["mark"])
+
+
+# - Give an example that requires you to store multiple types of data.
+a= [
+        20,
+        1,
+        3.0,
+        {
+            "name": "Ana",
+            "mark": 20
+        },
+        11,
+        'hello',
+        12,
+        [1,2,3,4],
+        [
+            {"k1":"v1"},
+            {},
+            []
+        ]
+]
+
+print(a[0])
+print(a[-1])
+print(a[-1][0]["k1"])
