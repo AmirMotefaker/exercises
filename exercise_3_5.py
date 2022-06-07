893532896eb38b3d50cd710e66f836a2478437ec
Amir Motefaker <amotef@gmail.com>
2022-06-07 10:27:40 +0430

    Create exercise_3_5.py

---
+++ exercise_3_5.py
@@ -0,0 +1,9 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #05
+
+# 5 - Calculate the following statement without using variables:
+# Multiply the number 2 by the power of 10 to the sum of the two 
+# numbers 15 and 3, and then multiply it by the result
+# Multiply the number 3 by the number 1000 (you can use parentheses)
+
+print(((2 ** 10) * (15 +(-3)) + (3 * 1000)))
