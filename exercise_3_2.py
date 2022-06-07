80a7482924a246f4a6b7c12dd6e6f436197e41ba
Amir Motefaker <amotef@gmail.com>
2022-06-07 09:55:52 +0430

    Create exercise_3_2.py

---
+++ exercise_3_2.py
@@ -0,0 +1,10 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #02
+
+# 2. Write the above program in such a way that the
+# result of this sum is first stored in a variable 
+# and then displayed by the print function.
+
+sum  = 23 + 45 +(-7)
+
+print(sum)
