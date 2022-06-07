d6b0232a9b237c780ded9fda320001c7f413cceb
Amir Motefaker <amotef@gmail.com>
2022-06-07 10:35:18 +0430

    Create exercise_3_7.py

---
+++ exercise_3_7.py
@@ -0,0 +1,7 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #07
+
+# 7- Without using a variable, multiply the number 14.3 by 3
+# and then return the result to the power of 3.
+
+print((3.14 * 3) ** 3)
