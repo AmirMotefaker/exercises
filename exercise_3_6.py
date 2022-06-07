71685fc10c92c159809abf04057a991863901de0
Amir Motefaker <amotef@gmail.com>
2022-06-07 10:31:36 +0430

    Create exercise_3_6.py

---
+++ exercise_3_6.py
@@ -0,0 +1,12 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #06
+
+# 6. Write the above program using variables.
+# Save the result of each operation in a separate variable.
+# Then save the final result in another variable and print it.
+
+a = 2 ** 10
+b = 15 +(-3)
+c = 3 * 1000
+
+print((a * b + c))
