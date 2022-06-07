6590a6919ffc4806634b3be3449b5af958571bbe
Amir Motefaker <amotef@gmail.com>
2022-06-07 09:55:27 +0430

    Create exercise_3_1.py

---
+++ exercise_3_1.py
@@ -0,0 +1,7 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #01
+
+# 1- Using Python, calculate the sum of these three numbers: 23, 45, -7
+# (Do this addition directly in print)
+
+print(23 + 45 +(-7))
