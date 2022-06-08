b5bdf4fb8871707013257b748e93e04ef7a8640f
Amir Motefaker <amotef@gmail.com>
2022-06-08 09:42:24 +0430

    Create exercise_3_14.py

---
+++ exercise_3_14.py
@@ -0,0 +1,9 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #14
+
+# 14. Count outside the division of the number 100 to 3.
+# (Hint: "out of the box" means how many numbers 3 are completely in the number 100)
+
+print(100 // 3)
+# output
+# 33
