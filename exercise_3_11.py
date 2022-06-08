d56fcdbd3091185aa866435d25a06cc69ece2a17
Amir Motefaker <amotef@gmail.com>
2022-06-08 09:29:46 +0430

    Create exercise_3_11.py

---
+++ exercise_3_11.py
@@ -0,0 +1,9 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #11
+
+# 11. What is the gender of the result of this mathematical expression? 
+# After guessing, test it with Python.
+
+print(1 + 2.5 - 3)
+# Output
+# 0.5
