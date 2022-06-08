041d86893e8d67907fc2ca3a5f8be3a759dea58f
Amir Motefaker <amotef@gmail.com>
2022-06-08 09:36:16 +0430

    Create exercise_3_12.py

---
+++ exercise_3_12.py
@@ -0,0 +1,9 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #12
+
+# 12. Calculate the exact value of dividing the number 115 by 2.
+# (That is, do a division that is a decimal place Display split result as well)
+
+print(115 / 2)
+# Output
+# 57.5
