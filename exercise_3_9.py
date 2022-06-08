4b621cdbe7a296732a5381ea3a49d2311df842b8
Amir Motefaker <amotef@gmail.com>
2022-06-08 09:00:06 +0430

    Update exercise_3_9.py
    Added comment

--- exercise_3_9.py
+++ exercise_3_9.py
@@ -1,6 +1,9 @@
 # Code by @Amir Motefaker
 # Exercises Chapter 3 #09
 
+# 9- What is the result of executing each of the following codes?
+#  (Based on operator priority)
+
 print(2 * 3 + 4)
 # output
 # 10
