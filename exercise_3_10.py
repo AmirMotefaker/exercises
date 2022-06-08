18342bed454734cd9cb139ef824bd5e8c149efc9
Amir Motefaker <amotef@gmail.com>
2022-06-08 09:09:19 +0430

    Create exercise_3_10.py

---
+++ exercise_3_10.py
@@ -0,0 +1,16 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #10
+
+# 10. In your opinion, if we divide the number 12 by 6,
+# the result will be 6 or 0.6? 
+# (Tip: The difference is that one is an integer and the other is a decimal number)
+# - Test the above item in Python.
+# - Check the result using the gender type function.
+
+print(12 / 6)
+# output
+# 2.0
+
+print(type(12 / 6))
+# output
+# <class 'float'>
