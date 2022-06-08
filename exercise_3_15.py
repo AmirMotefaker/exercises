85e17ba3bf06f63bd16c71031c54850813b162d3
Amir Motefaker <amotef@gmail.com>
2022-06-08 11:10:31 +0430

    Create exercise_3_15.py

---
+++ exercise_3_15.py
@@ -0,0 +1,18 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #15
+
+# 15. Save the values of both previous questions in two separate variables. 
+# Then multiply the variable that has the answer to the previous question 
+# by 3 and add the result to the variable that has the answer to the previous two questions.
+
+
+a = (100 % 3)
+b = (100 // 3)
+
+c = (b * 3) + a
+
+print(c)
+
+
+# Output 
+# 100
