5d6ccec93ff4c2161260d153e3bc4a0df90273cb
Amir Motefaker <amotef@gmail.com>
2022-06-09 08:37:03 +0430

    Create exercise_3_16.py

---
+++ exercise_3_16.py
@@ -0,0 +1,16 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #16
+
+# 16 - See the following code:
+# print (15 - 5 * 3 + 4)
+# By changing the code above, make sure that the value of 15 minus 5 is executed first and then the result is 3 to
+# Multiply by 4. (Tip: Using the correct parentheses)
+
+print(15 - 5 * 3 + 4)
+# output
+# 4
+
+
+print((15 - 5) * (3 + 4))
+# output
+# 70
