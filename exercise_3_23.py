1cb1b43a43d8f5171998b1278656c369aac41081
Amir Motefaker <amotef@gmail.com>
2022-06-10 03:00:18 +0430

    Create exercise_3_23.py

---
+++ exercise_3_23.py
@@ -0,0 +1,27 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #23
+
+# 23. By reading the following program, tell what this program does line by line.
+#   a = 2
+#   b = 3
+#   print (a ** (b + 1))
+#   name = "Mohammad Hadi"
+#   family_name = "HajiHosseini"
+#   print (name, family_name)
+#   job = "Developer"
+#   print (f'I am {name {{family_name}. I am a {job}. ')
+
+a = 2
+b = 3
+print (a ** (b + 1))
+name = "Mohammad Hadi"
+family_name = "HajiHosseini"
+print (name, family_name)
+job = "Developer"
+print(f'I am {name} {family_name}. I am a {job}. ')
+
+
+# output
+# 16
+# Mohammad Hadi HajiHosseini
+# I am Mohammad Hadi HajiHosseini. I am a Developer.
