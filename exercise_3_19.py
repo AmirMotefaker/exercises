89c587af2911ba28ff0005f4f977940101d0dd0c
Amir Motefaker <amotef@gmail.com>
2022-06-09 09:12:42 +0430

    Create exercise_3_19.py

---
+++ exercise_3_19.py
@@ -0,0 +1,32 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #19
+
+# 19 - Consider the following string.
+#   my_str = 'CodingYar.com'
+# • Select the letter n in this string, using indexing and display it.
+# • Select and display the letter m in two different ways.
+# • Using slicing, show the top string upside down.
+
+my_str = 'CodingYar.com'
+
+print(my_str[4])
+# output
+# n
+
+
+print(my_str[12])
+# output
+# m
+print(my_str[-1])
+# output
+# m
+
+print(my_str[::-1])
+# output
+# moc.raYgnidoC
+
+print ("".join(reversed(my_str)))
+# output
+# moc.raYgnidoC
+
+
