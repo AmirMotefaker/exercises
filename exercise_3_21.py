6a2c5c6607e57e634463d1152f4ac44c0c0af040
Amir Motefaker <amotef@gmail.com>
2022-06-10 02:51:25 +0430

    Create exercise_3_21.py

---
+++ exercise_3_21.py
@@ -0,0 +1,11 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #21
+
+# 21. The following string contains a series of numbers separated by commas.
+# Separate these numbers using string methods.
+#   my_str = '1,2,3,153,12,56,12,5'
+
+my_str = '1,2,3,153,12,56,12,5'
+
+my_str_out = my_str.split(',')
+print(my_str_out)
