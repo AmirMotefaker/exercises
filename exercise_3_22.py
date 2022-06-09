ddb840761c6783fca7e27e505fd9c36b4f22d2b5
Amir Motefaker <amotef@gmail.com>
2022-06-10 02:55:07 +0430

    Create exercise_3_22.py

---
+++ exercise_3_22.py
@@ -0,0 +1,12 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #22
+
+# 22. Create two variables and store a string in each. 
+# Now write a program that shows the two strings stuck together.
+# For example, if it has two strings "hello" and  The output of "python" is "hellopython".
+# Note that there is no space between these two words in the output.
+
+a = 'hello'
+b = 'python'
+
+print(a + b)
