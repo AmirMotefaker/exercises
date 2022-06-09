895d5f622f9ed06bd0e2084c950b3e8d2423e91a
Amir Motefaker <amotef@gmail.com>
2022-06-09 08:46:36 +0430

    Create exercise_3_18.py

---
+++ exercise_3_18.py
@@ -0,0 +1,10 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #18
+
+# 18- What mathematical expression does the following program count?
+# Try to identify it by checking the parentheses.
+#   (((4 * 2) ** 2) - (33/2) + 5) % 10
+
+print((((4 * 2) ** 2) - (33/2) + 5) % 10)
+# output
+# 2.5
