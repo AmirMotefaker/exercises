6747660e27332eff0e7e36c1a32dae7636c74c0a
Amir Motefaker <amotef@gmail.com>
2022-06-10 04:04:15 +0430

    Create exercise_3_27.py

---
+++ exercise_3_27.py
@@ -0,0 +1,13 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #27
+
+# 27. Make a list with 10 numbers stored in it.(Select the numbers of your choice)
+# now using the pop method, the third element (ie index number 2).
+# Remove from the list and save it in a variable. 
+# Then print that variable.
+
+my_list = ['5', '9', '-104', '14', '12', '10', '-1', '-4', '9', '78']
+print(len(my_list))
+
+my_list_pop = my_list.pop(2)
+print(my_list_pop)
