9e88d3aace1a50a2f1b68a5c398a9cd2f014181e
Amir Motefaker <amotef@gmail.com>
2022-06-09 08:40:55 +0430

    Create exercise_3_17.py

---
+++ exercise_3_17.py
@@ -0,0 +1,16 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #17
+
+# 17 - Save 20 in a variable called a and 110 in a variable called b.
+#  Create a variable called c and store the sum of the two variables 
+# a and b in it. Create a variable called d and store the two variables a and b in it.
+#  Then print the product of the two variables c and d.
+
+
+a = 20
+b = 110
+
+c = a + b
+d = a * b
+
+print(d * c)
