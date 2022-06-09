53c69b932fa487157bb2b126e6bc2015fd37da67
Amir Motefaker <amotef@gmail.com>
2022-06-10 03:49:23 +0430

    Create exercise_3_26.py

---
+++ exercise_3_26.py
@@ -0,0 +1,14 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #26
+
+# 26. Make a list that includes the numbers 5, 9, 14, 12, 10, -1, -4, and 9, respectively.
+# - Without using Python, just looking at the list you created, how long do you think this list is?
+# - Get the length of this list using Python.
+# - Without using Python, just looking at the list, what do you think is the last index of this list?
+# - Check the above by writing the Python code.
+
+my_list = ['5', '9', '14', '12', '10', '-1', '-4', '9']
+
+print(len(my_list))
+
+print(my_list[-1])
