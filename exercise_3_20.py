26a1d3b0fa073aa27c86eaa0f5a0340485641cfa
Amir Motefaker <amotef@gmail.com>
2022-06-09 09:16:12 +0430

    Create exercise_3_20.py

---
+++ exercise_3_20.py
@@ -0,0 +1,11 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #20
+
+# 20. What does Python do if we write the following phrase?
+#   my_str = 'hi' * 5
+#   print (my_str)
+
+my_str = 'hi' * 5
+print (my_str)
+# output
+# hihihihihi
