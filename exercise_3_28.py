647b5c1eb8982735a2f26bf40b071141aee36431
Amir Motefaker <amotef@gmail.com>
2022-06-10 04:08:15 +0430

    Create exercise_3_28.py

---
+++ exercise_3_28.py
@@ -0,0 +1,10 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #28
+
+# 28. Sort the list below.
+#   my_numbers = [5, 2, 8, -1, 2, 9, 0]
+
+my_numbers = [5, 2, 8, -1, 2, 9, 0]
+
+my_numbers.sort()
+print(my_numbers)
