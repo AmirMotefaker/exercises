b2b205d7ca348e7e37f418aae027d57065eabac4
Amir Motefaker <amotef@gmail.com>
2022-06-10 04:46:00 +0430

    Create exercise_3_29.py

---
+++ exercise_3_29.py
@@ -0,0 +1,15 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #29
+
+# 29. Using the indexes, access the "hello" element in the list below and change its value to "Python".
+#   my_list = [1, [1,2,3], ["hello", "hi", 10], 100]
+
+my_list = [1, [1,2,3], ["hello", "hi", 10], 100]
+
+print(my_list[2][0])
+# output
+# hello
+
+my_list[2][0] = "Python"
+print(my_list)
+
