d63706c2653703322249f6a5ec046b4158a2b679
Amir Motefaker <amotef@gmail.com>
2022-06-11 10:36:47 +0430

    Create exercise_3_33.py

---
+++ exercise_3_33.py
@@ -0,0 +1,8 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #33
+
+# 33. With what code can we access the index element 3 of this list?
+# my_list = [110,120,12, -1,84,23,2]
+
+my_list = [110,120,12, -1,84,23,2]
+print(my_list[3])
