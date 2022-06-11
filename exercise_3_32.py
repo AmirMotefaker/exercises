c1f16bfe5808b356c50731ee668b830f28cf6fad
Amir Motefaker <amotef@gmail.com>
2022-06-11 10:33:22 +0430

    Create exercise_3_32.py

---
+++ exercise_3_32.py
@@ -0,0 +1,11 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #32
+
+# 32. Make a list of 10 numbers.
+# Write a program that uses the min and max functions 
+# to print the largest and smallest number in the list.
+
+my_list = [14, -3, 1, 923, -32, 79, 61, 10, -723, 48]
+
+print(max(my_list))
+print(min(my_list))
