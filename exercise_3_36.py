bb532c86066123c5d92ac921734da9e045adaefd
Amir Motefaker <amotef@gmail.com>
2022-06-11 12:03:36 +0430

    Create exercise_3_36.py

---
+++ exercise_3_36.py
@@ -0,0 +1,18 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #36
+
+# 36. Make three variables with string, list and dictionary
+# genres and store the desired values in them. Now for each one, 
+# use the type () function to print its gender.
+
+a = "amir"
+b = [1, 2, 3, 4, 5]
+c = my_dict = {
+    "name": "amir",
+    "age": 43,
+    "programming_language": "python"
+}
+
+print(type(a))
+print(type(b))
+print(type(c))
