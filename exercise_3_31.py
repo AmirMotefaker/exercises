fbda06c8538bb0f3a2ff7b2cc0fb5dc12dce8cb9
Amir Motefaker <amotef@gmail.com>
2022-06-11 10:28:19 +0430

    Create exercise_3_31.py

---
+++ exercise_3_31.py
@@ -0,0 +1,12 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #31
+
+
+# 31. Make a list that contains 5 names. 
+# Now write code in the next line that replaces the second
+# element of the list (ie index number one) with your own name string.
+
+my_list = ["john", "apple", "hello", "germany", "table"]
+my_list[1] = "amir"
+
+print(my_list)
