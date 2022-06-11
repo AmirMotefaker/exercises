bcd64f15c60525275956e6423a0f430d355f0f75
Amir Motefaker <amotef@gmail.com>
2022-06-11 10:43:03 +0430

    Create exercise_3_34.py

---
+++ exercise_3_34.py
@@ -0,0 +1,20 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #34
+
+# 34 - How to access the value 100 in the following dictionaries?
+# - A (simple mode)
+# my_dict = {
+#  "key1": 100,
+#  "key2": 200,
+#  "key3": 300
+# }
+
+my_dict = {
+    "key1": 100,
+    "key2": 200,
+    "key3": 300
+}
+
+a = my_dict["key1"]
+print(a)
+
