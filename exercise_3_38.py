4e3751035ed428fdebc1a75b71b38dc98a2a0b20
Amir Motefaker <amotef@gmail.com>
2022-06-11 12:41:15 +0430

    Create exercise_3_38.py

---
+++ exercise_3_38.py
@@ -0,0 +1,28 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #38
+
+
+# 38. Consider the following dictionary.
+# person_details = {
+#  "name": "Ana",
+#  "age": 28,
+#  "languages": ["Python", "Java", "C"]
+#  }
+# - With what code can we get the name of the person from the following dictionary?
+# - With what code can we get the languages list of the above question?
+#    That is, with what code should we tell Python to return the list in front of the above dictionary languages?
+# - How can we access the third element of the languages list?
+
+ 
+person_details = {
+    "name": "Ana",
+    "age": 28,
+    "languages": ["Python", "Java", "C"]
+}
+
+
+print(person_details["name"])
+
+print(person_details["languages"])
+
+print(person_details["languages"][2])
