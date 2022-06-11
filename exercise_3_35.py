97b6b81f88501444e122588989171007ee4b18df
Amir Motefaker <amotef@gmail.com>
2022-06-11 11:57:15 +0430

    Create exercise_3_35.py

---
+++ exercise_3_35.py
@@ -0,0 +1,17 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 3 #35
+
+# 35. Create a dictionary that stores your name, your age, 
+# and your favorite programming language. 
+# Then, using print, write a program that displays all the 
+# dictionary keys at once and only the dictionary values once.
+
+my_dict = {
+    "name": "amir",
+    "age": 43,
+    "programming_language": "python"
+}
+
+print(my_dict.keys())
+
+print(my_dict.values())
