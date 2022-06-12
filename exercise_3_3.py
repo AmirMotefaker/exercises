f27401657664dcf394bee5e609d26ea91537f358
Amir Motefaker <amotef@gmail.com>
2022-06-12 09:08:12 +0430

    Update exercise_3_3.py
    Added solution 2 with output

--- exercise_3_3.py
+++ exercise_3_3.py
@@ -14,4 +14,16 @@
 # print(a / b)
 # print(a ** b)
 
-print((5 + ((2 - 3) * 2 / 4 ) ** 2 )) 
+print((5 + ((2 - 3) * 2 / 4 ) ** 2 ))
+# output 
+# 5.25
+
+
+print( ((2 + 3) ** 2) - 10 * 30 / 40)
+# output
+# 17.5
+
+# +- */ ** 
+# The first priority is with power,
+# then with multiplication and division,
+# and then with addition and subtraction
