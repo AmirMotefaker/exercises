b6bffb633ed5db84820a39a0e0b90ae3c837f2d9
Amir Motefaker <amotef@gmail.com>
2022-06-12 09:16:29 +0430

    Update exercise_3_4.py
    Added another solution with output

--- exercise_3_4.py
+++ exercise_3_4.py
@@ -4,3 +4,25 @@
 # 4 - Write a math phrase that uses the remaining and submultiple.
 
 print ((5 % 2) + (6 // 3) - (8 / 4))
+# output
+# 1.0
+
+print(100 % 50 )
+# output
+# 0
+
+print(102 % 50)
+# output
+# 2
+
+print(100 // 50)
+# output
+# 2
+
+print(100 / 50 )
+# output
+# 2.0
+
+print((101 // 5) + (1000 % 3))
+# output
+# 21
