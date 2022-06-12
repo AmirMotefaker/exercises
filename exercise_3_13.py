1a624e6541e5211c2f26cdd1646fd5c44213282a
Amir Motefaker <amotef@gmail.com>
2022-06-12 10:02:38 +0430

    Update exercise_3_13.py
    Added comment

--- exercise_3_13.py
+++ exercise_3_13.py
@@ -1,7 +1,7 @@
 # Code by @Amir Motefaker
 # Exercises Chapter 3 #13
 
-# 13. Calculate the remainder of dividing a number by 100.
+# 13. Calculate the remainder of dividing a 3 by 100.
 
 print ( 100 % 3)
 # output
