f6b601c5c50a55d48f1cde15764d752850f270b5
Amir Motefaker <amotef@gmail.com>
2022-06-12 13:19:22 +0430

    Udpate exercise_3_35.py
    Added solution 2 with output

--- exercise_3_35.py
+++ exercise_3_35.py
@@ -13,5 +13,22 @@ my_dict = {
 }
 
 print(my_dict.keys())
+# output:
+# dict_keys(['name', 'age', 'programming_language'])
 
 print(my_dict.values())
+# output:
+# dict_values(['amir', 43, 'python'])
+
+
+print(list(my_dict.keys()))
+# output:
+# ['name', 'age', 'programming_language']
+
+print(list(my_dict.values()))
+# output:
+# ['amir', 43, 'python']
+
+print(list(my_dict.items()))
+# output:
+# [('name', 'amir'), ('age', 43), ('programming_language', 'python')]
