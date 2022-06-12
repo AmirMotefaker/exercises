21c0ebd592526917429dbaf1d012b3097eff5b11
Amir Motefaker <amotef@gmail.com>
2022-06-12 09:31:49 +0430

    Update exercise_3_6.py
    Added solution 2 with output

--- exercise_3_6.py
+++ exercise_3_6.py
@@ -10,3 +10,24 @@ b = 15 +(-3)
 c = 3 * 1000
 
 print((a * b + c))
+
+# output
+# 15288
+
+
+z = 2
+x = 3
+y = 10
+p = 15
+q = 1000
+
+a = z ** y
+b = p -x
+c = a * b
+d = x * q
+result = c + d
+
+print(result)
+
+# output
+# 15288
