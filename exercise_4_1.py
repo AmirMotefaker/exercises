fb8734285d3920e0bf5e0426135fcbeb8066f2c9
Amir Motefaker <amotef@gmail.com>
2022-06-14 08:45:06 +0430

    Create exercise_4_1.py

---
+++ exercise_4_1.py
@@ -0,0 +1,13 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 4 #01
+
+# 1. Write a program that takes a number from the user.
+# Then tell the user whether the number is even or odd. 
+# (Tip: Do not forget to convert input to number)
+
+num = int(input("Enter a number: "))
+mod = num % 2
+if mod > 0:
+    print("This is an odd number.")
+else:
+    print("This is an even number.")
