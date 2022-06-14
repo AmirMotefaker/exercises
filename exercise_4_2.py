112c94e62fabbb35b24cde37015c72041570ec9c
Amir Motefaker <amotef@gmail.com>
2022-06-14 09:44:59 +0430

    Create exercise_4_2.py

---
+++ exercise_4_2.py
@@ -0,0 +1,22 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 4 #02
+
+# 2. Write a program that takes a word from the user. 
+# Then tell the user whether the length of the word is even or odd.
+
+name = str(input("Enter your name: "))
+
+length_name = len(name)%2
+
+if length_name == 0:
+	print(name, "even")
+else:
+	print(name, "odd")
+
+# output:
+# Enter your name: dssad231321
+# dssad231321 odd
+
+# output:
+# Enter your name: 123456$#%^amTR
+# 123456$#%^amTR even
