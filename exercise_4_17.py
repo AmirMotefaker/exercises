ec69702ccf5ca8885f3512d0df25008f43b06f1f
Amir Motefaker <amotef@gmail.com>
2022-06-15 11:56:50 +0430

    Create exercise_4_17.py

---
+++ exercise_4_17.py
@@ -0,0 +1,26 @@
+# Code by @Amir Motefaker
+# Exercises Chapter 4 #17
+
+# 17. Write the above program in such a way that if the name david
+#    is allowed in the participants then if the user enters the name
+#    david or even daVId he will still be told "you can log in".
+# That is, write the program in such a way that it is not sensitive to uppercase and lowercase letters.
+
+my_list = ['David', 'elli', 'backy']
+
+
+my_char = str(input("Enter name :"))
+if my_char in my_list:
+    print("You can log in.")
+else:
+    print("Nope, You cann't log in.")
+
+
+# output:
+# Enter name :david
+# Nope, You cann't log in.
+
+
+# output:
+# Enter name :David
+# You can log in.
