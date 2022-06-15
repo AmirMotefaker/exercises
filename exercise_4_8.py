b4ffaad2571d3ba36402de11a23138d71f14d5f0
Amir Motefaker <amotef@gmail.com>
2022-06-15 09:20:54 +0430

    Update exercise_4_8.py
    Added output

--- exercise_4_8.py
+++ exercise_4_8.py
@@ -5,7 +5,7 @@
 # Then display the number of days in that month to the user.
 # Example If the user enters the month Azar then print the number 30.
 
-month = str(input("Enter month: (f, o, k, t ,mo ,s, me, ab, az, d, b, e)"))
+month = str(input("Enter month: (f, o, k, t ,mo ,s, me, ab, az, d, b, e) "))
 
 if month == "f" or "o" or "k" or "t" or"mo" or "s":
     print("31")
@@ -14,3 +14,8 @@ elif month == "me" or "ab" or "az" or "d" or "b":
 
 else:
     print("29")
+
+
+# output:
+# Enter month: (f, o, k, t ,mo ,s, me, ab, az, d, b, e)me
+# 31
