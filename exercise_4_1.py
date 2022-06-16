fc2670a7c9bb53aa7e401f9da1ca489d2a1451c2
Amir Motefaker <amotef@gmail.com>
2022-06-16 09:20:07 +0430

    Update exercise_4_1.py
    Added solution 2 with output

--- exercise_4_1.py
+++ exercise_4_1.py
@@ -5,12 +5,12 @@
 # Then tell the user whether the number is even or odd. 
 # (Tip: Do not forget to convert input to number)
 
-num = int(input("Enter a number: "))
-mod = num % 2
-if mod > 0:
-    print("This is an odd number.")
-else:
-    print("This is an even number.")
+# num = int(input("Enter a number: "))
+# mod = num % 2
+# if mod > 0:
+#     print("This is an odd number.")
+# else:
+#     print("This is an even number.")
 
 
 # output:
@@ -20,3 +20,21 @@ else:
 # output:
 # Enter a number: 598
 # This is an even number.
+
+
+# Solution 2 - CodingYar
+my_number = int(input('Enter your number: '))
+
+if my_number % 2 == 0:
+    print('Even')
+else:
+    print('Odd')
+
+
+# output:
+# Enter your number: 23
+# Odd
+
+# output:
+# Enter your number: 132
+# Even
