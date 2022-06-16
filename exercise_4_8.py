59b11fb356341b9b1f63adae1c3c82f1e639cf22
Amir Motefaker <amotef@gmail.com>
2022-06-17 00:57:52 +0430

    Update exercise_4_8.py
    Added solution 2 and 3

--- exercise_4_8.py
+++ exercise_4_8.py
@@ -5,17 +5,59 @@
 # Then display the number of days in that month to the user.
 # Example If the user enters the month Azar then print the number 30.
 
-month = str(input("Enter month: (f, o, k, t ,mo ,s, me, ab, az, d, b, e) "))
+# month = str(input("Enter month: (f, o, k, t ,mo ,s, me, ab, az, d, b, e) "))
 
-if month == "f" or "o" or "k" or "t" or"mo" or "s":
-    print("31")
-elif month == "me" or "ab" or "az" or "d" or "b":
-    print("30")
+# if month == "f" or "o" or "k" or "t" or"mo" or "s":
+#     print("31")
+# elif month == "me" or "ab" or "az" or "d" or "b":
+#     print("30")
 
-else:
-    print("29")
+# else:
+#     print("29")
 
 
 # output:
 # Enter month: (f, o, k, t ,mo ,s, me, ab, az, d, b, e)me
 # 31
+
+
+# Solution 2 - CodingYar
+# month_name = input('Enter month name: ')
+# month_name = month_name.title()
+# # aZAR Azar aZar azar AzAr AZAR
+
+# months = {
+#     'Farvardin': 31,
+#     'Ordibehesht': 31,
+#     'Khordad': 31,
+#     'Tir': 31,
+#     'Mordad': 31,
+#     'Shahrivar': 31,
+#     'Mehr': 30,
+#     'Aban': 30,
+#     'Azar': 30,
+#     'Dey': 30,
+#     'Bahman': 30,
+#     'Esfand': 29
+# }
+
+# if month_name in months.keys():
+#     print(months[month_name])
+# else:
+#     print('Incorrect input')
+
+
+
+# Solution 3 - CodingYar
+month_name = input('Enter month name: ')
+month_name = month_name.lower()
+
+first_6_months = ['farvardin', 'ordibehesht', 'khordad', 'tir', 'mordad','shahrivar']
+if month_name in first_6_months:
+    print(31)
+else:
+    if month_name == 'esfand':
+        print(29)
+    else:
+        print(30)
+
