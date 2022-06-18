9c277cceda44fd4b1790dfcecefcd864982572a2
Amir Motefaker <amotef@gmail.com>
2022-06-18 10:45:58 +0430

    Update exercise_4_17.py
    Added solution 2 with lower()

--- exercise_4_17.py
+++ exercise_4_17.py
@@ -6,14 +6,14 @@
 #    david or even daVId he will still be told "you can log in".
 # That is, write the program in such a way that it is not sensitive to uppercase and lowercase letters.
 
-my_list = ['David', 'elli', 'backy']
+# my_list = ['David', 'elli', 'backy']
 
 
-my_char = str(input("Enter name :"))
-if my_char in my_list:
-    print("You can log in.")
-else:
-    print("Nope, You cann't log in.")
+# my_char = str(input("Enter name :"))
+# if my_char in my_list:
+#     print("You can log in.")
+# else:
+#     print("Nope, You cann't log in.")
 
 
 # output:
@@ -24,3 +24,28 @@ else:
 # output:
 # Enter name :David
 # You can log in.
+
+
+
+# Solution 2 - CodingYar
+names = ['Ana', 'John', 'david', 'cris', 'elli', 'Baxi']
+
+user_name = input('Enter your name: ')
+
+if user_name.lower() in names:
+    print('You can enter.')
+else:
+    print('No.')
+
+
+# Output:
+# Enter your name: david
+# You can enter.
+
+# Output:
+# Enter your name: DaviD
+# You can enter.
+
+# Output:
+# Enter your name: ali
+# No.
