9da996994deb9b4f0dca33c28484ffd81e9e167e
Amir Motefaker <amotef@gmail.com>
2022-06-18 09:41:16 +0430

    Update exercise_4_13.py
    Added solution 2 and 3 with output

--- exercise_4_13.py
+++ exercise_4_13.py
@@ -6,12 +6,12 @@
 #   If the user input is longer than one, tell the user that "input
 #   length is more than one" and the program will end. Otherwise say "input is correct"
 
-letter = str(input("Enter Letter :"))
+# letter = str(input("Enter Letter :"))
 
-if (len(letter) > 1):
-    print("Input length is more than one.")
-else:
-    print("The input is correct.")
+# if (len(letter) > 1):
+#     print("Input length is more than one.")
+# else:
+#     print("The input is correct.")
 
 # output:
 # Enter Letter :sd
@@ -20,3 +20,31 @@ else:
 # output:
 # Enter Letter :a
 # The input is correct.
+
+
+
+# Solution 2 - CodingYar
+# user_char = input('Enter your char: ')
+
+# if len(user_char) > 1 or len(user_char) ==0:
+#     print('Input length is exactly than one.')
+# else:
+#     print('Good.')
+
+
+# Solution 3 - CodingYar
+user_char = input('Enter your char: ')
+#len(user_char) > 1 or len(user_char) ==0
+if len(user_char) != 1 :
+    print('Input length is exactly than one.')
+else:
+    print('Good.')
+
+# Output:
+# Enter your char:
+# Input length is exactly than one.
+
+# Output:
+# Enter your char: a
+# Good.
+
