5ca4f28e68831c4e53ddd621676c414651dc9614
Amir Motefaker <amotef@gmail.com>
2022-06-18 11:01:17 +0430

    Update exercise_4_18.py
    Added solution 2 with output

--- exercise_4_18.py
+++ exercise_4_18.py
@@ -7,12 +7,12 @@
 # Example for abc show the letter b and for abcd show the letters bc to the user.
 
 
-name = input("Enter your name - ")
+# name = input("Enter your name - ")
  
-def middle_charracters(name):
-   return name[(len(name)-1)//2:(len(name)+2)//2]
+# def middle_charracters(name):
+#    return name[(len(name)-1)//2:(len(name)+2)//2]
  
-print(middle_charracters(name))
+# print(middle_charracters(name))
 
 
 # Output:
@@ -23,3 +23,34 @@ print(middle_charracters(name))
 # Output:
 # Enter your name - fdsfdfa
 # f
+
+
+# Solution 2 - CodingYar
+user_input = input('Enter your word: ')
+# abc    len:3   middle_index: 1
+# abcd   len:4   middle_index: 1,2
+
+word_len = len(user_input)
+
+if word_len == 0:
+   print('Lenght is zero.')
+else:
+   if word_len % 2 == 1:
+      index = int((word_len - 1)  / 2)
+      print(user_input[index])
+   else:
+      index = int(word_len / 2)
+      print(user_input[index-1:index+1])
+
+
+# Output:
+# Enter your word: abc
+# b
+
+# Output:
+# Enter your word: abcd
+# bc
+
+# Output:
+# Enter your word: 
+# Lenght is zero.
